988dfc896653cd72c96b56a9c1a97c66a9de07f4
Potato-Y <projectpotatoy@gmail.com>
2021-01-07 16:17:42 +0900

    Create 1.A+B 5.java

---
+++ JAVA/BAEKJOON Algorithm/단계별 문제/4.while문/1.A+B 5.java
@@ -0,0 +1,32 @@
+package MainFolder;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+
+        Scanner sc=new Scanner(System.in);
+        int a;
+        int b;
+
+        ArrayList<Integer> aList=new ArrayList<Integer>();
+        ArrayList<Integer> bList=new ArrayList<Integer>();
+        
+        while(true){
+            a=Integer.parseInt(sc.next());
+            b=Integer.parseInt(sc.next());
+            if(a==0&&b==0){
+                break;
+            }
+            aList.add(a);
+            bList.add(b);
+        }
+
+        for(int i=0;i<aList.size();i++){
+            System.out.println(aList.get(i)+bList.get(i));
+        }
+
+
+    }
+}
