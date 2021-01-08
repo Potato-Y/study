e5cc706a48d7657a33259199e17754a35504c6d0
Potato-Y <projectpotatoy@gmail.com>
2021-01-08 17:11:43 +0900

    Create 3.숫자의 갯수.java

---
+++ JAVA/BAEKJOON Algorithm/단계별 문제/5.1차원 배열/3.숫자의 갯수.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] num = new int[4];
+        String calNum;
+        for (int i = 0; i < 3; i++) {
+            num[i] = Integer.parseInt(sc.next());
+        }
+
+        num[3] = num[0] * num[2] * num[1];
+        calNum = Integer.toString(num[3]);
+
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int i1 = 0; i1 < calNum.length(); i1++) {
+                char sh = Integer.toString(i).charAt(0);
+                if (sh == calNum.charAt(i1)) count++;
+            }
+            System.out.println(count);
+            count = 0;
+        }
+
+
+    }
+}
