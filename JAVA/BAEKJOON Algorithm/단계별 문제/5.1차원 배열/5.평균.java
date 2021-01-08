848ab0bc55a71ff5af06bab8b7dfba4ae3c0c4b8
Potato-Y <projectpotatoy@gmail.com>
2021-01-08 19:58:00 +0900

    Create 5.평균.java

---
+++ JAVA/BAEKJOON Algorithm/단계별 문제/5.1차원 배열/5.평균.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int full = Integer.parseInt(sc.next());
+        double max = 0;
+
+        double[] num = new double[full];
+
+
+        for (int i = 0; i < full; i++) {
+            num[i] = Integer.parseInt(sc.next());
+            if (max < num[i]) max = num[i];
+        }
+
+        double allscore = 0;
+
+        for (int i = 0; i < full; i++) {
+            allscore = allscore + (num[i] * 100 / max);
+        }
+
+        System.out.println(allscore/full);
+    }
+}
