93ef0d7c298754849d5b8d865b8940ccad982d83
Potato-Y <projectpotatoy@gmail.com>
2021-01-08 18:35:06 +0900

    Create 4.나머지.java

---
+++ JAVA/BAEKJOON Algorithm/단계별 문제/5.1차원 배열/4.나머지.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] num = new int[10];
+        int[] calnum = new int[10];
+        for (int i = 0; i < 10; i++) {
+            num[i] = Integer.parseInt(sc.next());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            calnum[i] = num[i] % 42;
+        }
+
+        ArrayList<Integer> list=new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            for(int i1=0;i1<10;i1++){
+                if(!list.contains(calnum[i]))
+                if (calnum[i] == calnum[i1]) {
+                    list.add(calnum[i]);
+                    break;
+                }
+            }
+        }
+
+
+        System.out.println(list.size());
+    }
+}
