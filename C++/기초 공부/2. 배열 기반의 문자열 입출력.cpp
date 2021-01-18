dc501f4a966c0e0bd4406da2030b88f99d6cdbf6
Potato-Y <projectpotatoy@gmail.com>
2021-01-18 19:23:03 +0900

    Create 2. 배열 기반의 문자열 입출력.cpp

---
+++ C++/기초 공부/2. 배열 기반의 문자열 입출력.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+
+    char name[100];
+    char lang[200];
+
+    std::cout << "이름이 무엇입니까?" << std::endl;
+    std::cin >> name;
+
+    std::cout << "좋아하는 프로그래밍 언어는 무엇인가요?\n";
+    std::cin >> lang;
+
+    std::cout << "내 이름은 " << name << " 입니다.\n";
+    std::cout << "내가 좋아하는 프로그래밍 언어는 " << lang << "입니다.\n\n\n";
+
+    system("pause");
+    return 0;
+}
