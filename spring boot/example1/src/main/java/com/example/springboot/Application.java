ea8dd2c26ad04e1a808f08dbac44b41052d8f21c
Potato-Y <projectpotatoy@gmail.com>
2021-02-22 02:48:43 +0900

    spring boot p123

--- spring boot/example1/src/main/java/com/example/springboot/Application.java
+++ spring boot/example1/src/main/java/com/example/springboot/Application.java
@@ -2,7 +2,9 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 어노테이션들이 모두 활성화할 수 있도록 Application 클래스에 활성화 어노테이션을 추가
 @SpringBootApplication
 /*
 스프링부트의 자동 설정, 스프링 Bean읽기와 생성을 모두 자동으로 설정. 여기서부터 설정을 읽어나감
--- spring boot/example1/src/main/java/com/example/springboot/domain/posts/Posts.java
+++ spring boot/example1/src/main/java/com/example/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.springboot.domain.posts;
 
+import com.example.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter //클래스 내 모든 필드의 Getter 메소드를 자동으로 생성
 @NoArgsConstructor //기본 생성자 자동 추가
 @Entity  //테이블과 링크될 클래스임을 나타냄. 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭한다. SalesManager.java->sales_manager table
-public class Posts { //실제 DB의 테이블과 매칭될 클래스. Entity 클래스라고도 함
+public class Posts extends BaseTimeEntity { //실제 DB의 테이블과 매칭될 클래스. Entity 클래스라고도 함
     @Id  //해당 테이블의 PK 필드를 나타냅니다.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     //PK의 규칙을 나타냄. 스프링 부트 2.0에서는 GenerationType.IDENTITY 옵션을 추가해야만 한다.
@@ -35,5 +36,10 @@ public class Posts { //실제 DB의 테이블과 매칭될 클래스. Entity 클
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
