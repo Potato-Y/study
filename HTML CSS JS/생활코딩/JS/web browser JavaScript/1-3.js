d7d415733d743a4b66357afadd64d64df5e7bff0
Potato-Y <projectpotatoy@gmail.com>
2021-03-21 04:23:52 -0700

    JS 공부 추가

---
+++ HTML CSS JS/생활코딩/JS/web browser JavaScript/1-3.js
@@ -0,0 +1,4 @@
+var hg=document.getElementById('hg');
+hg.addEventListener('click', function(){
+    alert('Hello GitHub');
+})
---
+++ HTML CSS JS/생활코딩/JS/web browser JavaScript/1-4.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    var hg=document.getElementById('hg');
+    hg.addEventListener('click', function(){
+        alert('Hello GitHub');
+    })
+}
