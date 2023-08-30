1b8098d43b5d215bc2fbccfc60274160eaeaab96
Potato-Y <projectpotatoy@gmail.com>
2023-08-30 15:45:00 +0900

    중복되는 코드 정리

--- application/android_studio/activity_practice/app/src/main/java/com/example/activitypractice/MainActivity.java
+++ application/android_studio/activity_practice/app/src/main/java/com/example/activitypractice/MainActivity.java
@@ -23,49 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button buttonFileAccess = findViewById(R.id.buttonFileAccess); // file access 페이지를 여는 버튼 연결
+        setButtonOnclickListener(buttonFileAccess, FileAccessActivity.class);
+
         final Button buttonDateAndTime = findViewById(R.id.buttonDateAndTime); // date & time 페이지를 여는 버튼 연결
+        setButtonOnclickListener(buttonDateAndTime, DateAndTimeActivity.class);
+
         final Button buttonMenusAndDialogs = findViewById(R.id.buttonMenusAndDialogs); // Menu & Dialog 페이지를 여는 버튼 연결
+        setButtonOnclickListener(buttonMenusAndDialogs, MenusAndDialogsActivity.class);
+
         final Button buttonAutoComplete = findViewById(R.id.buttonAutoComplete); // 이하 생략
+        setButtonOnclickListener(buttonAutoComplete, AutoCompleteActivity.class);
+
         final Button buttonBarEx = findViewById(R.id.buttonBarEx);
+        setButtonOnclickListener(buttonBarEx, BarExActivity.class);
+
         final Button buttonViewContainer = findViewById(R.id.buttonViewContainer);
-
         setButtonOnclickListener(buttonViewContainer, ViewContainerActivity.class);
-
-        buttonBarEx.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setGoToPage(BarExActivity.class);
-            }
-        });
-
-        buttonAutoComplete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setGoToPage(AutoCompleteActivity.class);
-            }
-        });
-
-        buttonFileAccess.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FileAccessActivity.class);
-                startActivity(intent);
-            }
-        });
-        buttonDateAndTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), DateAndTimeActivity.class);
-                startActivity(intent);
-            }
-        });
-        buttonMenusAndDialogs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MenusAndDialogsActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     private void setButtonOnclickListener(Button button, Class<?> clazz) {
